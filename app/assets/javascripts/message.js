f99289bcc11d19f33b292ae27d2c1e165f76ff72
Ken-yoshi <Ken-yoshi@github.com>
2019-12-15 16:56:32 +0900

    chat-space 自動更新

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,51 +1,53 @@
 $(function() {
-  last_message_id = $(".message:last").data("messageId");
-  console.log(last_message_id);
      function buildHTML(message){
-      if (message.image) {
-          var html = 
-          `<div class = "message">
-            <div class = "upper-message">
-              <div class = "upper-message__user-name">
-                ${message.user_name}
-              </div>
-              <div class = "upper-message__date">
-                ${message.time}
-              </div>
-            </div>
-            <div class = "lower-message">
-              <p class = "lower-message__content">
-                ${message.content}
-              </p>
-            </div>
-            <img src="${message.image}" >
-          </div>`
+        let insertImage = '';
+        if (message.image) {
+            insertImage = `<img src="${message.image}">`;
+        }
+        let html =
+            `<div class = "message" data-message-id = "${message.id}">
+                <div class = "upper-message">
+                    <div class = "upper-message__user-name">
+                        ${message.user_name}
+                    </div>
+                    <div class = "upper-message__date">
+                        ${message.time}
+                    </div>
+                </div>
+                <div class = "lower-message">
+                    <p class = "lower-message__content">
+                        ${message.content}
+                    </p>
+                    ${insertImage}
+                </div>
+            </div>`;
         return html
-      } else {
-        var html =
-          `<div class = "message">
-            <div class = "upper-message">
-              <div class = "upper-message__user-name">
-                ${message.user_name}
-              </div>
-              <div class = "upper-message__date">
-                ${message.time}
-              </div>
-            </div>
-            <div class = "lower-message">
-              <p class = "lower-message__content">
-                ${message.content}
-              </p>
-            </div>
-          </div>`
-        return html
-      };
-    }
-    
+    };
+
+    let reloadMessages = function() {
+      let last_message_id = $(".message:last").data("message-id")
+      $.ajax( {
+        url: "api/messages",
+        type: "get",
+        dataType: "json",
+        data: {id: last_message_id}
+      })
+      .done(function(messages) {
+        let insertHTML = "";
+        $.each(messages, function(i, message) {
+          insertHTML += buildHTML(message)
+        });
+        $(".tweet").append(insertHTML);
+      })
+      .fail(function() {
+        alert("エラー");
+      });
+    };
+
   $("#new_message").on("submit", function(e){
     e.preventDefault();
-    var formData = new FormData(this);
-    var url = $(this).attr("action")
+    let formData = new FormData(this);
+    let url = $(this).attr("action")
     $.ajax({
       url: url,
       type: "POST",
@@ -55,14 +57,18 @@ $(function() {
       contentType: false
     })
     .done(function(data) {
-      var html = buildHTML(data);
-      $(".tweet").append(html)
-      $('.tweet').animate({ scrollTop: $('.tweet')[0].scrollHeight});
+      let html = buildHTML(data);
+      $(".tweet").append(html);
+      $('.tweet').animate({ scrollTop: $('.tweet')[0].scrollHeight}, "fast" );
       $("form")[0].reset();
       $( ".send_btn").prop( "disabled", false );
     })
     .fail(function() {
       alert("エラー");
     })
-  })
-});
+  });
+  $(function() {
+    if (location.pathname.match(/\/groups\/\d+\/messages/) )
+    setInterval(reloadMessages, 7000);
+  });
+ });
